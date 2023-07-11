b8d617714a5e425aaa08c9696259b19a3039afbb
minhcong <83624415+dmcong@users.noreply.github.com>
2023-07-11 16:06:49 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,6 @@ function App() {
           </Sider>
           <Content
             style={{
-              background: colorBgContainer,
               padding: 24,
             }}
           >
@@ -39,7 +38,7 @@ function App() {
           </Content>
         </Layout>
       </Content>
-      <Footer style={{ textAlign: 'center' }}>
+      <Footer style={{ textAlign: 'center', background: colorBgContainer }}>
         Desig Connector ©2023 Created by Desig Labs
       </Footer>
     </Layout>
